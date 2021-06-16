e559d0d369fce3e14f2621c1ec7f70f120248f93
Atheena Renjith <athenarenjit1@cet.ac.in>
2021-06-17 01:57:00 +0530

    Voter's list problem

---
+++ LanguageTools/votersList.cpp
@@ -0,0 +1,39 @@
+//voters'list problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> getVoters(int arr[], int n){
+    sort(arr, arr+n);
+    vector<int> result;
+    for(int i=0; i<n; ){
+         int count = 0;
+         int j = i;
+         while(j < n && ( arr[j] == arr[i] )){
+             count++;
+             j++;
+         }
+         if(count >= 2){
+             result.push_back(arr[i]);
+         }
+         i = j;
+    }
+    return result;
+}
+
+int main()
+{
+    int n1, n2, n3;
+    cin >> n1 >> n2 >> n3;
+    int total_elements = n1 + n2 + n3;
+    int arr[total_elements];
+    for(int i=0; i< total_elements; i++){
+        cin>> arr[i];
+    }
+    vector<int> voters = getVoters(arr, total_elements);
+    cout << voters.size();
+    for(int j = 0 ; j< voters.size() ; j++){
+        cout << voters[j] << endl;
+    }
+    return 0;
+}
