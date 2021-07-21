c75b4f262e83dfb6b930ac7aaf86cd0a946d80df
Atheena Renjith <athenarenjit1@cet.ac.in>
2021-07-21 11:54:50 +0530

    Add allIndices recursion problem.

--- BasicsOfRecursion/allIndices.cpp
+++ BasicsOfRecursion/allIndices.cpp
@@ -1,6 +1,18 @@
 int allIndexes(int a[], int size, int x, int output[]){
     if(size==0){
-        return -1;
+        return 0;
     }
-    
+    int ans  = allIndexes(a+1, size-1, x, output);
+    if(a[0] == x){
+        for(int i = ans-1; i>=0; i--){
+            output[i+1] = output[i] + 1;
+        }
+        output[0] = 0;
+        ans++;
+    }else{
+        for (int i = ans - 1; i >= 0; i--) {
+            output[i] = output[i] + 1;
+        }
+    }
+    return ans;
 }
