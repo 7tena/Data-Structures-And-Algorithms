5b9997d6c873d2793512364679dec9843f2ed662
Atheena Renjith <athenarenjit1@cet.ac.in>
2021-08-13 22:43:55 +0530

    Add height.cpp

---
+++ DS/Tree/height.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+};
+
+// recursive
+int height(Node *root){
+    if(root == NULL)
+        return -1; // to get 0 for leaf nodes
+    return max(height(root->left), height(root->right))+1; 
+}
