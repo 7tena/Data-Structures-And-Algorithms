0c43b260625fbbb7d0af5af94492c2826912f273
Atheena Renjith <athenarenjit1@cet.ac.in>
2021-09-16 22:22:28 +0530

    Check if subtree of another tree

---
+++ DS/Tree/subtreeofAnotherTree.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+class Solution {
+public:
+    bool isSameTree(TreeNode* a, TreeNode* b) {
+        if (a == NULL && b == NULL)
+        return true;
+        if (a != NULL && b != NULL)
+        {
+            return(
+            a->val == b->val &&
+            isSameTree(a->left, b->left) &&
+            isSameTree(a->right, b->right)
+            );
+        }
+        return false;
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root)
+            return false;
+        return isSameTree(root, subRoot) ||
+            isSubtree(root->left, subRoot)||
+            isSubtree(root->right, subRoot);
+    }
+};
