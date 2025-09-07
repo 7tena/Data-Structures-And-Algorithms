9d2edc8884c55191f8b76da391ae8deccae5884f
Atheena Renjith <athenarenjit1@cet.ac.in>
2025-09-07 12:08:13 +0530

    Create SumZero.java

---
+++ DS/Arrays/SumZero.java
@@ -0,0 +1,42 @@
+/*
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+Example 1:
+
+Input: n = 5
+Output: [-7,-1,1,3,4]
+Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+Example 2:
+
+Input: n = 3
+Output: [-1,0,1]
+Example 3:
+
+Input: n = 1
+Output: [0]
+ 
+
+Constraints:
+
+1 <= n <= 1000
+*/
+
+class Solution {
+    public int[] sumZero(int n) {
+        int[] res = new int[n];
+        int index = 0;
+        
+        // Fill with pairs (i, -i)
+        for (int i = 1; i <= n / 2; i++) {
+            res[index++] = i;
+            res[index++] = -i;
+        }
+        
+        // If n is odd, add a 0
+        if (n % 2 == 1) {
+            res[index] = 0;
+        }
+        
+        return res;
+    }
+}
